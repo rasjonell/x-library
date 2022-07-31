d69e687d8f589e0c978b442fb879af8660dcf778
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2022-07-31 15:21:51 +0400

    Add Ecto.Changeset validations to ErrorView

--- lib/books_api_web/views/error_view.ex
+++ lib/books_api_web/views/error_view.ex
@@ -10,7 +10,20 @@ defmodule BooksApiWeb.ErrorView do
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.json" becomes
   # "Not Found".
-  def template_not_found(template, _assigns) do
-    %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
+  def template_not_found(template, %{error: error}) do
+    %{
+      errors: %{
+        error: get_errors(error),
+        detail: Phoenix.Controller.status_message_from_template(template)
+      }
+    }
+  end
+
+  defp get_errors(changeset) do
+    Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
+      Regex.replace(~r"%{(\w+)}", msg, fn _, key ->
+        opts |> Keyword.get(String.to_existing_atom(key), key) |> to_string()
+      end)
+    end)
   end
 end
